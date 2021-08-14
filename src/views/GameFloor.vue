aa943cc4975b49c0234fff1f5e70dbad428f9c08
unknown <d.dogan@etu.edu.tr>
2021-08-14 15:51:33 +0300

    Adding Game Floor view

---
+++ src/views/GameFloor.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="green-table"></div>
+</template>
+
+<script>
+export default {
+  name: "MainFloor",
+};
+</script>
+
+<style>
+.green-table {
+  display: flex;
+  justify-content: space-evenly;
+  height: 100vh;
+  width: 100vw;
+  border-width: initial;
+  border-style: none;
+  border-color: initial;
+  border-image: initial;
+  padding: 0px;
+  background-image: radial-gradient(
+    rgba(57, 172, 57, 0.725),
+    rgb(0, 116, 0),
+    rgb(0, 100, 0)
+  );
+}
+</style>
