7bbd6a47c13eb3d97b2a88164e94c00fb58e3d94
unknown <d.dogan@etu.edu.tr>
2021-08-14 15:00:25 +0300

    Creating PlayingCard component

---
+++ src/components/PlayingCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <template v-if="card.showFront">
+      <v-row>
+        <v-col cols="cols">
+          <img class="card-image" :src="card.image" />
+        </v-col>
+      </v-row>
+    </template>
+    <template v-else>
+      <v-row>
+        <v-col cols="cols">
+          <img class="card-image" src="card.backImage" />
+        </v-col>
+      </v-row>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlayingCard",
+  props: ["card"],
+  data() {
+    return {
+      cols: 12,
+    };
+  },
+};
+</script>
+
+<style>
+.card-image {
+  border-radius: 8px;
+}
+</style>
