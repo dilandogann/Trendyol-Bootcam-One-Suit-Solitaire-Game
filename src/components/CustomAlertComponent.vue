2e0ed65ec9c4de36284d25a969fd60bc5bd1d072
unknown <d.dogan@etu.edu.tr>
2021-08-14 22:45:50 +0300

    Adding custom alert component for handling errors

---
+++ src/components/CustomAlertComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="error.show" width="500">
+      <v-alert type="error" class="error" dense>
+        {{ error.message }}
+      </v-alert>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+  name: "CustomAlertComponent",
+  props: {
+    error: {
+      type: Object,
+      default() {
+        return { show: false, message: "" };
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+.error{margin-bottom: 0;}
+</style>
