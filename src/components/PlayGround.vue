120381b498fe4be06e915067889806dd8911ac98
unknown <d.dogan@etu.edu.tr>
2021-08-14 18:58:04 +0300

    Bug fix

--- src/components/PlayGround.vue
+++ src/components/PlayGround.vue
@@ -1,29 +1,5 @@
 <template>
-  <v-container style="margin-top: 100px">
-    <v-row>
-      <v-col cols="1"></v-col>
-      <v-col
-        v-for="(playingCardSuit, suitIndex) in playingCards"
-        :key="suitIndex"
-      >
-        <v-row v-for="playingCard in playingCardSuit" :key="playingCard.id">
-          <playing-card
-            :card="playingCard"
-            animation="200"
-          />
-        </v-row>
-      </v-col>
-      <v-col cols="1"></v-col>
-    </v-row>
-    <div
-      class="floorCards cursor"
-      v-for="card in floorCards"
-      :key="card.id"
-      @click="dealFloorCards"
-    >
-      <playing-card :card="card"> </playing-card>
-    </div>
-  </v-container>
+  <div>Play Ground</div>
 </template>
 
 <script>
@@ -100,25 +76,25 @@ export default {
         if (filtered.length === 0) return card;
       });
     },
-  },
-  //Creating 6-6-6-6-5-5-5-5-5-5 chunks of playingCards array
-  chunkPlayingCards() {
-    const overflowingItems = this.playingCards.slice(-4);
-    this.playingCards = this.playingCards.slice(
-      0,
-      this.playingCards.length - 4
-    );
-    this.playingCards = _.chunk(this.playingCards, 5);
-    for (let i = 0; i < overflowingItems.length; i++) {
-      this.playingCards[i].push(overflowingItems[i]);
-    }
-  },
-  //It changes last card's of chunks showFront property in order to showing front side
-  showFrontSideOfLastCardsInChunks() {
-    for (let i = 0; i < this.playingCards.length; i++) {
-      const length = this.playingCards[i].length;
-      this.playingCards[i][length - 1].showFront = true;
-    }
+    //Creating 6-6-6-6-5-5-5-5-5-5 chunks of playingCards array
+    chunkPlayingCards() {
+      const overflowingItems = this.playingCards.slice(-4);
+      this.playingCards = this.playingCards.slice(
+        0,
+        this.playingCards.length - 4
+      );
+      this.playingCards = _.chunk(this.playingCards, 5);
+      for (let i = 0; i < overflowingItems.length; i++) {
+        this.playingCards[i].push(overflowingItems[i]);
+      }
+    },
+    //It changes last card's of chunks showFront property in order to showing front side
+    showFrontSideOfLastCardsInChunks() {
+      for (let i = 0; i < this.playingCards.length; i++) {
+        const length = this.playingCards[i].length;
+        this.playingCards[i][length - 1].showFront = true;
+      }
+    },
   },
 };
 </script>
